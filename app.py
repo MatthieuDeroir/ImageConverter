3bd3cfa977840acce7c313da0e2d6e2b1d43d1b6
MatthieuDeroir <matthieu.deroir@epsi.fr>
2023-08-04 22:59:24 +0200

    multiple file upload

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, request, send_from_directory
+from flask import Flask, render_template, request, send_from_directory, send_file
 from PIL import Image
+import zipfile
 import os
 
 app = Flask(__name__)
@@ -13,15 +14,22 @@ def convert_to_webp(input_path, output_path):
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
-        file = request.files['file']
-        if file:
-            input_path = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
-            output_path = os.path.splitext(input_path)[0] + '.webp'
-            file.save(input_path)
-            convert_to_webp(input_path, output_path)
-            return send_from_directory(app.config['UPLOAD_FOLDER'], output_path.split('/')[-1], as_attachment=True)
+        files = request.files.getlist('file')
+        if files:
+            # Create a ZIP file to hold the converted images
+            zip_path = os.path.join(app.config['UPLOAD_FOLDER'], 'converted_images.zip')
+            with zipfile.ZipFile(zip_path, 'w') as zipf:
+                for file in files:
+                    input_path = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+                    output_path = os.path.splitext(input_path)[0] + '.webp'
+                    file.save(input_path)
+                    convert_to_webp(input_path, output_path)
+                    # Add the converted file to the ZIP
+                    zipf.write(output_path, os.path.basename(output_path))
+            return send_file(zip_path, as_attachment=True, download_name='converted_images.zip')
 
     return render_template('upload.html')
+
 
 if __name__ == '__main__':
     os.makedirs(UPLOAD_FOLDER, exist_ok=True)
